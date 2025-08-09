03b3c6a834d77926507a6d3bc8a536274534f41b
Sean Murphy <sean@iamseanmurphy.com>
2025-08-09 16:19:18 +0000

    2.13.1

--- force-refresh/vendor/composer/installed.php
+++ force-refresh/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'jordanleven/force-refresh',
-        'pretty_version' => 'v2.13.0',
-        'version' => '2.13.0.0',
-        'reference' => 'ff4c1f663da83966ad492e5e3e8956ed20e92eb8',
+        'pretty_version' => 'v2.13.1',
+        'version' => '2.13.1.0',
+        'reference' => '95078b30cefbe899dff59966e620dd7a202800d9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'jordanleven/force-refresh' => array(
-            'pretty_version' => 'v2.13.0',
-            'version' => '2.13.0.0',
-            'reference' => 'ff4c1f663da83966ad492e5e3e8956ed20e92eb8',
+            'pretty_version' => 'v2.13.1',
+            'version' => '2.13.1.0',
+            'reference' => '95078b30cefbe899dff59966e620dd7a202800d9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
