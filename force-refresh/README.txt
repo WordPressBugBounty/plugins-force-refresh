3eb603789597a1c891ac890fdd812df22eb162a4
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 04:34:32 +0000

    2.14.0

--- force-refresh/README.txt
+++ force-refresh/README.txt
@@ -1,10 +1,10 @@
 === Force Refresh ===
-Stable tag: 2.13.2
+Stable tag: 2.14.0
 Contributors: jordanleven
 Tags: refresh, reload, single page application
 Requires PHP: 7.4
 Requires at least: 5.2
-Tested up to: 6.8
+Tested up to: 6.9
 License: GPLv2 or later
 Force Refresh is a simple plugin that allows you to force a page refresh for users currently visiting your site on a per-page or post basis.
 
@@ -34,6 +34,12 @@ Upload the Force Refresh plugin to your site, and then simply activate it. To fo
 5. If you're having trouble, clicking on "Troubleshooting" will allow you to view the current settings for Force Refresh and allow you to enter Debugging Mode — an enhanced browser console logging mode that can be used to diagnose issues.
 
 == Changelog ==
+#### 2.14.0
+_Released on November 29, 2025_
+
+##### **New Features**
+* Add support for WordPress 6.9.
+
 #### 2.13.2
 _Released on August 29, 2025_
 
--- force-refresh/vendor/composer/installed.php
+++ force-refresh/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'jordanleven/force-refresh',
-        'pretty_version' => 'v2.13.2',
-        'version' => '2.13.2.0',
-        'reference' => 'e488e61518ab12480f8dbc9ef10b0552c7c21e94',
+        'pretty_version' => 'v2.14.0',
+        'version' => '2.14.0.0',
+        'reference' => '14de6c8131e95643fe47cc418fc7145b4d0aabcf',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'jordanleven/force-refresh' => array(
-            'pretty_version' => 'v2.13.2',
-            'version' => '2.13.2.0',
-            'reference' => 'e488e61518ab12480f8dbc9ef10b0552c7c21e94',
+            'pretty_version' => 'v2.14.0',
+            'version' => '2.14.0.0',
+            'reference' => '14de6c8131e95643fe47cc418fc7145b4d0aabcf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
